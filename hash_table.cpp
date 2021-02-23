7a871bef743ce6fc0aa7739cee8180f90a0260a5
Piyush Singh <piyushsinghkgpian@gmail.com>
2021-02-23 16:17:20 +0530

    Update hash_table.cpp

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,5 +1,5 @@
 // Hash Table Implementation using Linked Lists with chaining
-// Has O(1) Search
+// Has O(1) average search complexity
 
 #include <iostream>
 
@@ -30,7 +30,7 @@ public:
 
 	HashTable(int size){
 		this->size=size;
-		this->ht = new LinkedListNode*[size]; //array of pointers
+		this->ht = new LinkedListNode*[size]; //array of pointers, length of array=#indices in HT, we have a LL at each index
 		for (int i = 0; i < size; i++) {
 			this->ht[i] = NULL;
 		}
@@ -46,7 +46,9 @@ public:
 			this->ht[index] = createNode(key, value);
 		} else {
 			LinkedListNode *curr = this->ht[index];
-			while(curr->next!=NULL){
+			// std::cout << "Sth there" << '\n' << "Present:" << curr->value << " Incoming:" << value << '\n';
+			if (curr->key==key) {this->ht[index] = createNode(key, value); return;}
+			while(curr->next!=NULL && curr->next->key != key){
 				curr = curr->next;
 			}
 			curr->next = createNode(key, value);
@@ -54,49 +56,48 @@ public:
 		return;
 	};
 
-	void Search(int key){
+	bool Search(int key){
 		int index = hashFunction(key);
 		if (this->ht[index]==NULL){
 			std::cout << "Key Not Found." << '\n';
-			return;
+			return 0;
 		}
 		LinkedListNode *curr = this->ht[index];
 		while(curr!=NULL){
+			std::cout << "Traversing LL to look for " << key << " Curr:" << curr->key << '\n';
 			if (curr->key==key) {
 				std::cout << "Key Found with value " << curr->value << '\n';
-				return;
+				return 1;
 			}
 			curr = curr->next;
 		}
-		std::cout << "Key Not Found." << '\n';
-		return;
+		std::cout << "Index found but Key Not Found." << '\n';
+		return 0;
 	};
 
 	void Delete(int key){
+		if (!Search(key)){ std::cout << "Key Not Found." << '\n'; return; }
+
 		int index = hashFunction(key);
-		if (this->ht[index]==NULL){
-			std::cout << "Key Not Found." << '\n';
-			return;
-		}
+		if (this->ht[index]->key==key){ this->ht[index]=this->ht[index]->next; return;  }
+
 		LinkedListNode *curr = this->ht[index];
-		if (curr->key==key) {
-			std::cout << "Key Found with value " << curr->value << '\n';
-			curr = NULL;
-			return;
-		}
-		LinkedListNode *prev = curr;
-		curr = curr->next;
-		while(curr!=NULL){
-			if (curr->key==key) {
-				std::cout << "Key Found with value " << curr->value << '\n';
-				prev->next = curr->next;
-				return;
+		while (curr->next->key!=key) curr = curr->next;
+		curr->next = curr->next->next;
+		return;
+	};
+
+	void Display(){
+		std::cout << "Current HashTable" << '\n';
+		for (int idx=0; idx<this->size; idx++) {
+			std::cout << idx << ": [";
+			LinkedListNode *curr = this->ht[idx];
+			while(curr!=NULL){
+				std::cout << '{' << curr->key << ':' << curr->value <<'}' << ',';
+				curr = curr->next;
 			}
-			prev =  prev->next;
-			curr = curr->next;
-			return;
+			std::cout << ']' << '\n';
 		}
-		std::cout << "Key Not Found." << '\n';
 		return;
 	};
 
@@ -117,35 +118,45 @@ void test(){
 int  main(){
 	HashTable ht =  HashTable(20);
 	int key, value;
-	int c;
+	int choice;
 	while (1){
 		std::cout << "Enter 1 for Insertion," << ' ' <<
 		"2 for Search," << ' ' <<
 		"3 for Deletion," << ' ' <<
-		"4 for Exit." << '\n';
-		std::cin>>c;
-		switch (c) {
+		"4 for Display," << ' ' <<
+		"5 for Exit." << '\n';
+		if (!(std::cin>>choice)){
+			std::cin.clear();
+			while (std::cin.get()!= '\n');
+			std::cout << "Invalid Input data type!\n";
+			continue;
+		}
+		switch (choice) {
 			case 1:
-				std::cout<<"Enter Key"<<'\n';
+				std::cout<<"Enter Key to Insert"<<'\n';
 				std::cin>>key;
 				std::cout<<"Enter Value"<<'\n';
 				std::cin>>value;
 				ht.Insert(key, value);
 				break;
 			case 2:
-				std::cout<<"Enter Key"<<'\n';
+				std::cout<<"Enter Key to Search"<<'\n';
 				std::cin>>key;
 				ht.Search(key);
 				break;
 			case 3:
-				std::cout<<"Enter Key"<<'\n';
+				std::cout<<"Enter Key to Delete"<<'\n';
 				std::cin>>key;
 				ht.Delete(key);
 				break;
 			case 4:
+				ht.Display();
+				break;
+			case 5:
 				exit(1);
 			default:
-			std::cout<<"Enter a value from 1 to 4"<<'\n';
+				std::cout<<"Enter an int from 1 to 5 as choice"<<'\n';
+				break;
 		}
 	}
 	return 0;
